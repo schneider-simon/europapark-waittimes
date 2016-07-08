55d77f9d0e3ed79575041ebfbec456562e636c95
Simon Schneider <sschneider@w-commerce.de>
2016-07-08 15:22:19 +0200

    Fixed Bugs

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 var request = require('request');
 var crypto = require('crypto');
 var moment = require('moment');
-var fs = require('fs');
 var momentTimezone = require('moment-timezone');
+var fs = require('fs');
 
 var mockWaiTimes = require('./data/waittimes.json');
 
-var mode = "live";
+var mode = "real";
 
-var time = moment.unix(1467973025).tz('Europe/Berlin');
+var time = moment.tz('Europe/Berlin');
 var dateString = time.format('YYYYMMDDHHmm');
-var code = crypto.createHash('md5').update('Europa-Park' + dateString + 'Webservice').digest('hex');
+var code = crypto.createHash('md5').update('Europa-Park' + dateString + 'SecondTry').digest('hex');
 
 console.log(dateString);
 
 var parameters = {
     code: code,
-    v: 2,
-    dc: time.format('x')
+    v: 4,
+    base: time.format('X')
 };
+
 console.log(parameters);
 
 if(mode === "test"){
@@ -32,8 +33,10 @@ if(mode === "test"){
 }
 
 function loaded(result){
-    var json = JSON.stringify(JSON.parse(result), null, 4);
 
-    fs.writeFile("./data/fetched/wait-times_" + moment().format('YYYY-MM-DD-HH-mm-ss') + '.json', json);
+    var json = (typeof result !== 'object')? JSON.parse(result) : result;
+    var jsonString = JSON.stringify(json, null, 4);
+
+    fs.writeFile("./data/fetched/wait-times_" + moment().format('YYYY-MM-DD-HH-mm-ss') + '.json', jsonString);
 
 }
